b09b4514a2edef0975f1f3e44006ed45d59e6ea5
Eugene Levenetc <shumvlesu@gmail.com>
2020-02-20 19:03:07 +0100

    Update 2: b

--- app/src/main/java/com/elevenetc/playground/android/quality/sample/MainActivity.kt
+++ app/src/main/java/com/elevenetc/playground/android/quality/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // update 2 - a
+// update 2 - b
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
